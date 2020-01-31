677304ff076f09c50205b0595228fa1c2ad707df
luosiyu <1514813390@qq.com>
2020-01-31 18:15:22 +0800

    update

---
+++ components/common/price.vue
@@ -0,0 +1,23 @@
+<template>
+	<view class="d-flex main-text-color font-md line-h">
+		<text class="a-self-start" :class="unitSize">￥</text><slot />
+	</view>
+</template>
+
+<script>
+	export default {
+		props:{
+			priceSize: {
+				type: String,
+				default: "font-md"
+			},
+			unitSize: {
+				type: String,
+				default: "font-md"
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
---
+++ pages/detail/detail.vue
@@ -0,0 +1,60 @@
+<template>
+	<view>
+		<!-- banner -->
+		<swiper-image :resdata="banners" height="750" preview></swiper-image>
+		
+		<!-- 基础详情 -->
+		<view class="p-2">
+			<view class="font-lg">
+				我是标题
+			</view>
+			<view class="font text-light-muted mb-3 line-h-sm">
+				我是描述
+			</view>
+			<price priceSize="font-lg" unitSize="font">3500</price>
+		</view>
+		
+		<!-- 滚动商品特性 -->
+		
+		<!-- 属性选择 -->
+		
+		<!-- 横向滚动评论 -->
+		
+		<!-- 商品详情 -->
+		
+		<!-- 热门推荐 -->
+		
+	</view>
+</template>
+
+<script>
+import swiperImage from "@/components/index/swiper-image.vue"	
+import price from '@/components/common/price.vue';	
+export default {
+	components:{
+		swiperImage,price
+	},
+	data() {
+		return {
+			preview:[
+				'https://i8.mifile.cn/v1/a1/6d13e060-d5c5-e610-88d0-80816fa2b0ce.webp','https://i8.mifile.cn/v1/a1/aff0b715-e63a-68f3-91b0-46a22543555e.webp'
+			],
+			banners:[
+				{ src: 'https://i8.mifile.cn/v1/a1/6d13e060-d5c5-e610-88d0-80816fa2b0ce.webp'},
+				{ src: 'https://i8.mifile.cn/v1/a1/aff0b715-e63a-68f3-91b0-46a22543555e.webp'}
+			],
+		}
+	},
+	onLoad() {
+		
+	},
+	methods: {
+		
+		
+	}
+}
+</script>
+
+<style>
+
+</style>
